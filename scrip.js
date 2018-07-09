66614226234ba360dbdeec122e96af2ca50603c2
rejiserbrazzers <40791904+rejiserbrazzers@users.noreply.github.com>
2018-07-09 11:38:39 +0300

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -2,10 +2,131 @@ function checkWinner(){
     var allblock = document.getElementsByClassName('block');
     //console.log(allblock);
     
-    if(allblock[0].innerHTML=='x' && allblock[1].innerHTML=='x' && allblock[2] =='x')
+    if(allblock[0].innerHTML=='x' && allblock[1].innerHTML=='x' && allblock[2].innerHTML =='x' )
         {
             alert("X WINS");
         }
+    
+  
+    if(allblock[3].innerHTML == 'x' && allblock[4].innerHTML=='x' && allblock[5].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+
+    if(allblock[6].innerHTML == 'x' && allblock[7].innerHTML=='x' && allblock[8].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+
+     if(allblock[0].innerHTML == 'x' && allblock[3].innerHTML=='x' && allblock[6].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+     if(allblock[1].innerHTML == 'x' && allblock[4].innerHTML=='x' && allblock[7].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+     if(allblock[2].innerHTML == 'x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+        
+      if(allblock[0].innerHTML == 'x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+      if(allblock[4].innerHTML == 'x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+      if(allblock[8].innerHTML == 'x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+    
+    
+    
+      if(allblock[0].innerHTML == 'x' && allblock[4].innerHTML=='x' && allblock[8].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+      if(allblock[2].innerHTML == 'x' && allblock[4].innerHTML=='x' && allblock[6].innerHTML=='x')
+        {
+                    alert("X WINS");
+        }
+    
+    
+    
+    //////////////////////////////////////////////////////////////////////////////
+    
+    
+    
+     if(allblock[0].innerHTML=='O' && allblock[1].innerHTML=='O' && allblock[2].innerHTML =='O' )
+        {
+            alert("O WINS");
+        }
+    
+  
+    if(allblock[3].innerHTML == 'O' && allblock[4].innerHTML=='O' && allblock[5].innerHTML=='O')
+        {
+                 alert("O WINS");
+        }
+
+    if(allblock[6].innerHTML == 'O' && allblock[7].innerHTML=='O' && allblock[8].innerHTML=='O')
+        {
+                    alert("O WINS");
+        }
+
+     if(allblock[0].innerHTML == 'O' && allblock[3].innerHTML=='O' && allblock[6].innerHTML=='O')
+        {
+                   alert("O WINS");
+        }
+     if(allblock[1].innerHTML == 'O' && allblock[4].innerHTML=='O' && allblock[7].innerHTML=='O')
+        {
+                    alert("O WINS");
+        }
+     if(allblock[2].innerHTML == 'O' && allblock[5].innerHTML=='O' && allblock[8].innerHTML=='O')
+        {
+                    alert("O WINS");
+        }
+        
+      if(allblock[0].innerHTML == 'O' && allblock[5].innerHTML=='O' && allblock[8].innerHTML=='O')
+        {
+                    alert("O WINS");
+        }
+      if(allblock[4].innerHTML == 'O' && allblock[5].innerHTML=='O' && allblock[8].innerHTML=='O')
+        {
+                     alert("O WINS");
+        }
+      if(allblock[8].innerHTML == 'O' && allblock[5].innerHTML=='O' && allblock[8].innerHTML=='O')
+        {
+                    alert("O WINS");
+        }
+    
+      if(allblock[0].innerHTML == 'O' && allblock[4].innerHTML=='O' && allblock[8].innerHTML=='O')
+        {
+                   alert("O WINS");
+        }
+      if(allblock[2].innerHTML == 'O' && allblock[4].innerHTML=='O' && allblock[6].innerHTML=='O')
+        {
+                     alert("O WINS");
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
   window.onload = function(){
 for (var i=0; i<9 ; i++){
@@ -31,8 +152,7 @@ document.getElementById('gam').onclick = function(event){
            checkWinner();
         }
       
-         
-    checkWinner();
+
  
         }
 }
